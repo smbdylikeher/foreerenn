431338510ffe10e020940580f20dc4ffbd681b48
smbdylikeher <ferdifebriank4646@gmail.com>
2025-10-20 22:14:25 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let meteorid = document.getElementById('meteorid');
 let rocket = document.getElementById('rocket');
 let text = document.getElementById('text');
 let button = document.getElementById('button');
+let music = document.getElementById('music');
 
 window.addEventListener('scroll', function(){
     let value = window.scrollY;
@@ -11,3 +12,14 @@ window.addEventListener('scroll', function(){
     text.style.marginBottom = value * 1 + 'px';
     button.style.marginBottom = value * 1 + 'px';
 });
+
+// Fungsi agar musik bisa dimainkan setelah user klik tombol Explore
+function playMusic() {
+    if (music.paused) {
+        music.play().catch(err => {
+            console.log("Playback prevented:", err);
+        });
+    } else {
+        music.pause();
+    }
+}
